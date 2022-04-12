66cc15ef51a160c146e155c870ed3df3afa7ea65
JBShort <81993517+JBShort@users.noreply.github.com>
2022-04-11 23:12:45 -0700

    fixing API links

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       <div class="weather-wrap" v-if="typeof weather.main !='undefined'">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }} </div>
-          <div class="date">Monday 20 January 2020</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
           <div class="temp"> {{ Math.round(weather.main.temp) }} </div>
@@ -31,22 +31,32 @@ export default {
   data () {
     return {
       api_key: '92687dc82e40f1fba84f789e1da90cf7',
-      url_base: "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API key}",
+      url_base: "https://api.openweathermap.org/data/2.5/",
       query: '',
-      weather: {}
+      weather: {},
+      city: {}
     }
   },
   methods: {
     fetchWeather (e) {
       if (e.key == "Enter") {
-        fetch('${this.url_base}weather?q=${this.query}&APPID=${this.api_key}')
+        fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=92687dc82e40f1fba84f789e1da90cf7')
+          .then(city => {
+            return city.json();
+          }) .then(this.setCity);
+        fetch('${this.url_base}weather?q=${this.query}&appid=${this.api_key}')
           .then(res => {
             return res.json();
           }).then(this.setResults);
       }
     },
+    setCity (city) {
+      this.city = city;
+      console.log(this.city)
+    },
     setResults (results) {
       this.weather = results;
+      console.log(this.weather);
     },
     dateBuilder () {
       let d = new Date();
@@ -57,7 +67,7 @@ export default {
       let date = d.getDate();
       let month = months[d.getMonth()];
       let year = d.getFullYear();
-      
+
       return `${day} ${date} ${month} ${year}`;
     }
   }
